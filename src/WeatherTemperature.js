1314bfa40b8d11d1d131e9add7a3c8abffab7a4b
JLL-5199 <113879978+JLL-5199@users.noreply.github.com>
2024-06-22 17:31:44 +0200

    remove Fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,31 +1,11 @@
 import React, {useState} from "react";
 
 export default function WeatherTemperature(props){
-    const [unit, setUnit] = useState("celsius")
-
-    function convertFahrenheit(event){
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function convertCelsius(event){
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    if (unit==='celsius'){
+  
 
     return(
         <div className="temperature">
-            <span className="degree">{Math.round(props.celsius)}</span><span className="units">°C | <a href="/" onClick={convertFahrenheit}>°F</a></span>
+            <span className="degree">{Math.round(props.celsius)}</span><span className="units">°C </span>
         </div>
     )
-} else {
-    let fahrenheit =(props.celsius *9)/5+32;
-    return(
-        <div className="temperature">
-            <span className="degree">{Math.round(fahrenheit)}   </span><span className="units"><a href="/" onClick={convertCelsius}>°C</a> | °F </span>
-        </div>
-    );
-}
 }
